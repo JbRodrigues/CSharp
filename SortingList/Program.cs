183b95a6d344c4dd17482b6f5cf231b8cc93396a
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2023-04-17 19:20:03 -0300

    Add sorted array

--- SortingList/Program.cs
+++ SortingList/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-// about the console template and the set of environment variables available.
-
-using System;
+﻿using System;
 
 namespace CSharp
 {
@@ -9,25 +6,43 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            //5,3,6,2,10
-            Console.WriteLine(findLower());
+            //Declares the main array
+            int[] arr = { 5, 3, 6, 2, 10 };
+            //Declares the lower variable and assing the value with a call of a function that find the lower value of the array
+            int lower = findLower(arr);
+            //Call the function that sort the array and build a new array with number already sorted
+            sortArray(arr, lower);
+            //Write on screen the array with commas
+            Console.WriteLine(string.Join(",", arr));
         }
 
-        static int findLower()
+        //Function that find the lower value of the array
+        static int findLower(int[] arr)
         {
-            int[] lowerArr = { 5, 3, 6, 2, 10 };
-            int lower = lowerArr[0];
-            int lowerIndex = 0;
-            for (int i = 0; i < lowerArr.Length; i++)
+            //Variable assing with the first element of the main array
+            int lower = arr[0];
+            for (int i = 1; i < arr.Length; i++)
             {
-                if (lowerArr[i] < lower)
+                //if the value of the element in 'arr[i]' position is lower then the value of lower, then assign this value of variable lower
+                if (arr[i] < lower)
                 {
-                    lower = lowerArr[i];
-                    lowerIndex = i;
+                    lower = arr[i];
                 }
             }
-
+            //retunr only ONE element of the array with lower value.
             return lower;
+        }
+
+        static void sortArray(int[] arr, int lower)
+        {
+            Array.Sort(arr);
+            if (arr[0] != lower)
+            {
+                int index = Array.IndexOf(arr, lower);
+                int temp = arr[0];
+                arr[0] = arr[index];
+                arr[index] = temp;
+            }
         }
     }
 }
